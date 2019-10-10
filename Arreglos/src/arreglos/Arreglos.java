5e06ed14c1725bfcd294f53e87cf1b3c3bea7e66
carlos <chgutierrez18@misena.edu.co>
2019-10-10 16:29:19 -0500

    Arreglos Carlos Gey

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,27 @@
+
+package arreglos;
+
+import java.util.Scanner;
+
+public class Arreglos {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner (System.in);
+        
+        int [] Notas = new int[5];
+        
+        for (int Estud = 0; Estud <= 4; Estud++) {
+            System.out.println("Ingrese la calificacion del estudiante N: " + (Estud + 1) + " : ");
+            Notas[Estud] = input.nextInt();
+        }
+        
+        for (int Estud = 0; Estud <= 4; Estud++) {
+            System.out.println("Estudiante N: " + (Estud + 1) + " : " + Notas[Estud]);
+            
+        }
+    }
+}   
+
+    
